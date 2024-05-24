1414c8f967ea82cbb4791277d214a67f8e8548a3
edmondhabimana <edmond.habimana@yahoo.com>
2024-05-24 15:46:49 -0700

    style error component

--- src/features/error/Error.jsx
+++ src/features/error/Error.jsx
@@ -7,6 +7,17 @@ import { faArrowLeftLong } from "@fortawesome/pro-regular-svg-icons";
 const LeftArrowIcon = styled(FontAwesomeIcon)`
   margin-right: 5px;
 `
+const ErrorContainer = styled.div`
+  background-color: #2d3438;
+  height: 100vh;
+  color: white;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`
 
 export default function Error() {
   const navigate = useNavigate()
@@ -18,13 +29,13 @@ export default function Error() {
   }
 
   return(
-    <div>
+    <ErrorContainer>
       <h1>Something went wrong</h1>
       <p>{error.data || error.message}</p>
       <BackButton onClick={handleNavigation}>
         <LeftArrowIcon icon={faArrowLeftLong} />
         back
       </BackButton>
-    </div>
+    </ErrorContainer>
   )
 }
